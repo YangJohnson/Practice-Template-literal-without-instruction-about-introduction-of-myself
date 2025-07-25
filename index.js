79c0d1377c8e9ef39686018e38c4e557bc0312a5
YangJohnson <Yangjhnsn@outlook.com>
2025-07-25 15:35:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ const eco8 = 'beach';
 const eco9 = 'cleanup';
 const eco10 = 'cleanup';
 const eco11 = 'time';
-let introduction = (`Hello, My name is ${firstName} ${lastName} I'm ${age} years old this year. I live in ${district} and I'm a ${religion} who regularly attended church! The fun fact about myself is i always say ${eco1} to ${eco2} use ${eco3} in order for our ${eco4} to live in a ${eco5} ${eco6}. I was involve in Monthly ${eco7} and annual ${eco8} ${eco9}! I also do ${eco10} during my free ${eco11} on weekend sometimes!`);
+let introduction = (`Hello, My name is ${firstName} ${lastName} I'm ${age} years old this year. I live in ${district} and I'm a ${religion} who regularly attended church! The fun fact about myself is i always say ${eco1} to ${eco2} use ${eco3} in order for our future ${eco4} to live in a ${eco5} ${eco6}. I was involve in Monthly ${eco7} and annual ${eco8} ${eco9}! I also do ${eco10} during my free ${eco11} on weekend sometimes!`);
 console.log(introduction);
 
