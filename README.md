8c1b46adede5f39af4ebc56cab330213a0d9e07b
YangJohnson <Yangjhnsn@outlook.com>
2025-07-25 15:30:34 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Practice-Template-literal-without-instruction-about-introduction-of-myself
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/YangJohnson/Practice-Template-literal-without-instruction-about-introduction-of-myself)
---
+++ index.js
@@ -0,0 +1,21 @@
+// run `node index.js` in the terminal
+
+const firstName = 'Yang';
+const lastName = 'Johnson';
+let age = '28';
+const district = 'Kota Kemuning';
+const religion = 'Christian';
+const eco1 = 'No';
+const eco2 = 'single';
+const eco3 = 'plastic';
+const eco4 = 'generation';
+const eco5 = 'better';
+const eco6 = 'world';
+const eco7 = 'plogging';
+const eco8 = 'beach';
+const eco9 = 'cleanup';
+const eco10 = 'cleanup';
+const eco11 = 'time';
+let introduction = (`Hello, My name is ${firstName} ${lastName} I'm ${age} years old this year. I live in ${district} and I'm a ${religion} who regularly attended church! The fun fact about myself is i always say ${eco1} to ${eco2} use ${eco3} in order for our ${eco4} to live in a ${eco5} ${eco6}. I was involve in Monthly ${eco7} and annual ${eco8} ${eco9}! I also do ${eco10} during my free ${eco11} on weekend sometimes!`);
+console.log(introduction);
+
